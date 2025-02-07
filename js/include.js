215f65052ac9f3179e4badd7acc5514c40343111
BrentOpsomer <98383047+BrentOpsomer@users.noreply.github.com>
2025-02-07 15:46:29 +0100

    Update include.js fix

--- js/include.js
+++ js/include.js
@@ -1,1 +1,99 @@
-const _0x2744ff=_0x516a;(function(_0x1c4e57,_0x104f7e){const _0x4f91d0=_0x516a,_0x29831e=_0x1c4e57();while(!![]){try{const _0xf89cf1=parseInt(_0x4f91d0(0x101))/0x1+-parseInt(_0x4f91d0(0xfa))/0x2*(parseInt(_0x4f91d0(0x100))/0x3)+-parseInt(_0x4f91d0(0xfc))/0x4*(parseInt(_0x4f91d0(0x104))/0x5)+parseInt(_0x4f91d0(0x102))/0x6*(-parseInt(_0x4f91d0(0xfd))/0x7)+-parseInt(_0x4f91d0(0xf9))/0x8+parseInt(_0x4f91d0(0x103))/0x9*(-parseInt(_0x4f91d0(0xf6))/0xa)+parseInt(_0x4f91d0(0xf7))/0xb;if(_0xf89cf1===_0x104f7e)break;else _0x29831e['push'](_0x29831e['shift']());}catch(_0x4a4a5a){_0x29831e['push'](_0x29831e['shift']());}}}(_0x5295,0x64189));function _0x516a(_0x1360cc,_0x356aa7){const _0x529535=_0x5295();return _0x516a=function(_0x516aec,_0x4bdd0f){_0x516aec=_0x516aec-0xf5;let _0x19d7b0=_0x529535[_0x516aec];return _0x19d7b0;},_0x516a(_0x1360cc,_0x356aa7);}function _0x5295(){const _0x2bdf99=['126dfAulx','4003165TKhMIO','\x0a\x20\x20\x20\x20\x0a\x0a\x20\x20\x20\x20<link\x20rel=\x22stylesheet\x22\x20href=\x22css/taakbalk.css\x22>\x0a\x20\x20\x20\x20<link\x20rel=\x22stylesheet\x22\x20href=\x22css/achtergrond.css\x22>\x0a\x20\x20\x20\x20<link\x20rel=\x22stylesheet\x22\x20href=\x22css/zijmenu.css\x22>\x0a\x20\x20\x20\x20<link\x20rel=\x22stylesheet\x22\x20href=\x22https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\x22>\x0a\x20\x20\x20\x20\x0a\x0a\x20\x20\x20\x20<nav\x20class=\x22navigatie\x22>\x0a\x20\x20\x20\x20<div\x20class=\x22bedrijfstitel\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<img\x20src=\x22imagesoriginal/logogroot.png\x22\x20onclick=\x22location.href=\x27http://127.0.0.1:5501/index.html\x27\x22\x20alt=\x22Bedrijfslogo\x22>\x0a\x20\x20\x20\x20</div>\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20<a\x20href=\x22#\x22\x20class=\x22zijKnop\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22bar\x22></span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22bar\x22></span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22bar\x22></span>\x0a\x20\x20\x20\x20\x20\x20</a>\x20\x0a\x20\x20\x20\x20\x20<div\x20class=\x22navigatieLinks\x22>\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20<ul>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li><a\x20href=\x22index.html\x22>Openingstijden</a></li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li><a\x20href=\x22assortiment.html\x22>Assortiment</a></li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li\x20class=\x22winkelmandLi\x22><a\x20href=\x22#contact\x22\x20onclick=\x22openZijMenu()\x22>Contact</a></li>\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20</ul>\x0a\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20</nav>\x0a\x20\x20\x20\x20<div\x20id=\x22side-menu\x22\x20class=\x22menu\x22\x20ontouchstart=\x22menuTouchStart(event)\x22\x20ontouchmove=\x22menuTouchMove(event)\x22\x20ontouchend=\x22menuTouchEnd(event)\x22\x20bestellen>\x0a\x20\x20\x20\x20\x20\x20<a\x20href=\x22#\x22\x20class=\x22sluitKnop\x22\x20onclick=\x22sluitZijMenu()\x22>&times;</a>\x0a\x20\x20\x20\x20\x20\x20<!--\x20prod\x201\x20-->\x0a\x20\x20\x20\x20\x20\x20<div\x20class=\x22sidelogoPlace\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<img\x20class=\x22sidelogo\x22\x20src=\x22imagesoriginal/bakkersamlogo.png\x22>\x0a\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20<div\x20class=\x22contact\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<h2\x20class=\x22contactName\x22><i>Bakker\x20Sam</i></h2>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<!--\x20𝑎𝑘𝑘𝑒𝑟\x20𝑆𝑎𝑚\x20-->\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p\x20class=\x22bnb\x22>Brood\x20&\x20banket</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p\x20class=\x22tell\x22>TEL\x20:\x20<a\x20class=\x22nummerT\x22\x20href=\x22tel:055-31-28-98\x22>055/31-28-98</a>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p\x20class=\x22contactMail\x22>Email\x20:\x20<a\x20class=\x22mail\x22\x20href\x20=\x20\x22mailto:\x20sam.opsomer@telenet.be\x22>sam.opsomer@telenet.be</a></p>\x0a\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20<div\x20class=\x22bestellen\x20active\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<h2\x20class=\x22reviewG\x22>Geef\x20ons\x20een\x20review\x20op</h2>\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<a\x20href=\x22https://www.google.com/search?q=bakker+sam&rlz=1C1QPHC_nlBE969BE969&oq=bakker+sam&aqs=chrome.0.35i39i355j46i39i175i199j46i175i199i512j0i512j46i175i199i512j69i60l3.1800j0j4&sourceid=chrome&ie=UTF-8\x22\x20target=\x22_blank\x22><img\x20class=\x22google\x22\x20src=\x22imagesoriginal/google.png\x22></a>\x0a\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20<script\x20src=\x22taakbalk.js\x22></script>\x0a\x20\x20\x20\x20<script\x20src=\x22js/zijmenu.js\x22>\x20</script>\x0a\x20\x20\x20\x20','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sideproductencontainer\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li\x20class=\x22sideproducten\x22\x20onclick=\x22openZijProducten()\x22><a\x20class=\x22opena\x22>lll</a></li>\x0a\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20<div\x20id=\x22zijContact\x22\x20class=\x22zijContact\x22\x20ontouchstart=\x22zijContactTouchStart(event)\x22\x20ontouchmove=\x22zijContactTouchMove(event)\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<a\x20class=\x22closebtn\x22\x20onclick=\x22sluitZijProducten()\x22>&times;</a>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<h1>Assortiment</h1>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<ul\x20class=\x22zijprod\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li\x20class=\x22libox\x22>\x20<a\x20class=\x22productzijtext\x22\x20href=\x22brood.html\x22>Brood</a></li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li><a\x20class=\x22productzijtext\x22\x20href=\x22speciaalbrood.html\x22></i>Speciaalbrood</a></li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li><a\x20class=\x22productzijtext\x22\x20href=\x22koekebrood.html\x22></i>Koekebrood</a></li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li><a\x20class=\x22productzijtext\x22\x20href=\x22koffiekoeken.html\x22></i>Koffiekoeken</a></li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li><a\x20class=\x22productzijtext\x22\x20href=\x22donuts.html\x22></i>Donuts</a></li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li><a\x20class=\x22productzijtext\x22\x20href=\x22kleinbrood.html\x22></i>Kleinbrood</a></li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li><a\x20class=\x22productzijtext\x22\x20href=\x22gebak.html\x22></i>Gebak</a></li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<li><a\x20class=\x22productzijtext\x22\x20href=\x22taarten.html\x22></i>Taarten</a></li>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</ul>\x0a\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20','getElementById','369080JvvYEp','19468889hTaLVM','innerHTML','721800SjwYkj','4MHbhoq','addEventListener','4DnBwfe','3052091bLFurh','sideProducts-placeholder','\x0a\x20\x20<div\x20class=\x22copyPos\x22>\x0a\x20\x20<p\x20class=\x22copyright\x22>Copyright\x20©\x202025\x20Bakker\x20Sam.\x20Alle\x20rechten\x20voorbehouden</p>\x0a</div>\x0a\x20\x20\x20\x20','261078olTRWp','657729KEZRnZ','6RExNyF'];_0x5295=function(){return _0x2bdf99;};return _0x5295();}function insertNavigationMenu(){const _0x2db537=_0x516a,_0x235318=_0x2db537(0x105),_0x401af3=document[_0x2db537(0xf5)]('navigation-placeholder');_0x401af3&&(_0x401af3[_0x2db537(0xf8)]=_0x235318);}function includeFooterMenu(){const _0xabe1fc=_0x516a,_0x12af6b=_0xabe1fc(0xff),_0x49a714=document[_0xabe1fc(0xf5)]('footer-placeholder');_0x49a714&&(_0x49a714[_0xabe1fc(0xf8)]=_0x12af6b);}function includeSideProducts(){const _0x4d06bf=_0x516a,_0x4340b3=_0x4d06bf(0x106),_0x35f1ac=document[_0x4d06bf(0xf5)](_0x4d06bf(0xfe));_0x35f1ac&&(_0x35f1ac['innerHTML']=_0x4340b3);}document[_0x2744ff(0xfb)]('DOMContentLoaded',()=>{insertNavigationMenu(),includeFooterMenu(),includeSideProducts();});
+// Function to generate and insert the navigation menu
+function insertNavigationMenu() {
+  const navigationMenu = `
+    
+    <link rel="stylesheet" href="css/taakbalk.css">
+    <link rel="stylesheet" href="css/achtergrond.css">
+    <link rel="stylesheet" href="css/zijmenu.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <nav class="navigatie">
+      <div class="bedrijfstitel"><img onclick="location.href='index'" src="./imagesOriginal/logogroot.png"></div>
+      <a href="#" class="zijKnop">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </a> 
+     <div class="navigatieLinks"> 
+        <ul>
+          <li><a href="index.html">Openingstijden</a></li>
+          <li><a href="assortiment.html">Assortiment</a></li>
+          <li class="winkelmandLi"><a href="#contact" onclick="openZijMenu()">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div id="side-menu" class="menu" ontouchstart="menuTouchStart(event)" ontouchmove="menuTouchMove(event)" ontouchend="menuTouchEnd(event)" bestellen>
+      <a href="#" class="sluitKnop" onclick="sluitZijMenu()">&times;</a>
+      <!-- prod 1 -->
+      <div class="sidelogoPlace">
+        <img class="sidelogo" src="/imagesOriginal/bakkersamlogo.png">
+      </div>
+      <div class="contact">
+          <h2 class="contactName"><i>Bakker Sam</i></h2>
+          <!-- 𝑎𝑘𝑘𝑒𝑟 𝑆𝑎𝑚 -->
+          <p class="bnb">Brood & banket</p>
+          <p class="tell">TEL : <a class="nummerT" href="tel:055-31-28-98">055/31-28-98</a>
+          <p class="contactMail">Email : <a class="mail" href = "mailto: sam.opsomer@telenet.be">sam.opsomer@telenet.be</a></p>
+    
+      </div>
+      
+      
+      <div class="bestellen active">
+              <h2 class="reviewG">Geef ons een review op</h2> 
+              <a href="https://www.google.com/search?q=bakker+sam&rlz=1C1QPHC_nlBE969BE969&oq=bakker+sam&aqs=chrome.0.35i39i355j46i39i175i199j46i175i199i512j0i512j46i175i199i512j69i60l3.1800j0j4&sourceid=chrome&ie=UTF-8" target="_blank"><img class="google" src="imagesOriginal/google.png"></a>
+      </div>
+    </div>
+    <script src="taakbalk.js"></script>
+    <script src="js/zijmenu.js"> </script>
+    `;
+  // Get the element where you want to insert the navigation menu (e.g., the <header> element)
+  const headerElement = document.getElementById('navigation-placeholder');
+  // Insert the navigation menu HTML into the <header> element
+  if (headerElement) {
+    headerElement.innerHTML = navigationMenu;
+  }
+}
+// Function to generate and insert the footer menu
+function includeFooterMenu() {
+  const footerMenu = `
+  <div class="copyPos">
+  <p class="copyright">Copyright © 2024 Bakker Sam. Alle rechten voorbehouden</p>
+  <p class="privacy"><a class="privacyLink" href="privacyBeleid.html">Privacy Policy</a></p>
+</div>
+    `;
+  const footerElement = document.getElementById('footer-placeholder');
+  if (footerElement) {
+    footerElement.innerHTML = footerMenu;
+  }
+}
+function includeSideProducts() {
+  const sideProductsMenu = `
+          <div class="sideproductencontainer">
+          <li class="sideproducten" onclick="openZijProducten()"><a class="opena">lll</a></li>
+      </div>
+      <div id="zijContact" class="zijContact" ontouchstart="zijContactTouchStart(event)" ontouchmove="zijContactTouchMove(event)">
+          <a class="closebtn" onclick="sluitZijProducten()">&times;</a>
+          <h1>Assortiment</h1>
+          <ul class="zijprod">
+              <li class="libox"> <a class="productzijtext" href="brood.html">Brood</a></li>
+              <li><a class="productzijtext" href="speciaalbrood.html"></i>Speciaalbrood</a></li>
+              <li><a class="productzijtext" href="koekebrood.html"></i>Koekebrood</a></li>
+              <li><a class="productzijtext" href="koffiekoeken.html"></i>Koffiekoeken</a></li>
+              <li><a class="productzijtext" href="donuts.html"></i>Donuts</a></li>
+              <li><a class="productzijtext" href="kleinbrood.html"></i>Kleinbrood</a></li>
+              <li><a class="productzijtext" href="gebak.html"></i>Gebak</a></li>
+              <li><a class="productzijtext" href="taarten.html"></i>Taarten</a></li>
+          </ul>
+      </div>
+    `;
+  const sideProductsElement = document.getElementById('sideProducts-placeholder');
+  if (sideProductsElement) {
+    sideProductsElement.innerHTML = sideProductsMenu;
+  }
+}
+// Call the functions to insert the navigation and footer menus when the DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+  insertNavigationMenu();
+  includeFooterMenu();
+  includeSideProducts();
+});
